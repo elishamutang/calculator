c4b80e9b9669c0acdca1caa7dc1a2914364747a7
elishamutang <elishamutang@outlook.com>
2023-09-20 22:31:41 +0800

    Added operate function, edited individual arithmetic functions.

--- calculator.js
+++ calculator.js
@@ -1,28 +1,35 @@
-// Operations (addition, subtraction, multiplication and division)
-function add(operations) {
-    const input = operations.split("");
-    return parseInt(input[0]) + parseInt(input[2]);
+// first, second (addition, subtraction, multiplication and division)
+function add(first, second) {
+    return first+second;
 }
 
-function subtract(operations) {
-    const input = operations.split("");
-    return parseInt(input[0]) - parseInt(input[2]);
+function subtract(first, second) {
+    return first-second;
 }
 
-function multiply(operations) {
-    const input = operations.split("");
-    return parseInt(input[0]) * parseInt(input[2]);
+function multiply(first, second) {
+    return first*second;
 }
 
-function divide(operations) {
-    const input = operations.split("");
-    return parseInt(input[0]) / parseInt(input[2]);
+function divide(first, second) {
+    return first/second;
 }
 
 // Declaring first number, operator and second number.
 let firstNum, operator, secondNum;
 
-console.log(add("3+5"));
-console.log(subtract("3+5"));
-console.log(multiply("3+5"));
-console.log(divide("3+5"));
+// Operate function: takes an operator and 2 numbers and calls one of the above functions
+function operate(first, operator, second) {
+    if(operator == "+") {
+        result = add(first, second);
+    } else if(operator == "-") {
+        result = subtract(first, second);
+    } else if(operator == "*") {
+        result = multiply(first, second);
+    } else {
+        result = divide(first, second);
+    }
+    return result;
+};
+
+console.log(operate(1,"+",3));
