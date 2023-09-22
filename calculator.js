1947d04e4488029ac371408deebe8720dfa87c34
elishamutang <elishamutang@outlook.com>
2023-09-22 20:51:20 +0800

    Removed JS for generating numpad rows.

--- calculator.js
+++ calculator.js
@@ -33,18 +33,3 @@ function operate(first, operator, second) {
 };
 
 console.log(operate(1,"+",3));
-
-
-// Calculator body
-// Generating numpad
-
-let calBody = document.getElementById("calBody");
-
-// Available numpad width of 400px
-const rows = 400/80;
-
-for(let i=0; i<rows; i++) {
-    let calNumpad = document.createElement("div");
-    calNumpad.className = "numpad";
-    calBody.append(calNumpad);
-}
